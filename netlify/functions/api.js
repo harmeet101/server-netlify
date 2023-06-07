b3ef52fc23f00e2e616c189e7ab171fbb5772577
Harmeet Kaur <harmeet@andromedasecurity.com>
2023-06-07 16:04:28 +0530

    Initial setup

---
+++ netlify/functions/api.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const api = express();
+
+const router = express.Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
+module.exports = api;
+module.exports.handler = serverless(api);
